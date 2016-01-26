f822278d182e9925bd2abf5dc5a5b5fe20316d55
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-01-26 21:34:00 +0000

    remove series, things inside astricies and some others

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ var films = fs.readFileSync('./FilmsDB.csv', 'utf8').toString().split('\n');
 
 var rules = [
   /\(.*\)/g, // matches everthing in brackets
+  /\*.*\*/g, // matches everthing in astrisks
+  /\(.*[\s]?$/g, // matches everthing in brackets or something with one bracket
+  /\*/g, // matches astrisks
   /\b[\s+]?[-]?[\s+]?THE[\s+]?[-]?$/g, // mathes [- THE, THE ...] at end
   /\b[\s+]?[-]?[\s+]?AN[\s+]?[-]?$/g, // mathes [- AN, AN ...] at end
-  /\*/g // matches astrisks
+  /\b[\s+]?[-]?[\s+]?A[\s+]?[-]?$/g, // mathes [- A, A ...] at end
+  /[\s+]?[-]?[\s+]?$/g // trailing hyphans
 ];
+var isShow = function(str) {
+  return (
+    /SERIES [\d]/.test(str) ||
+    /SEASON [\d]/.test(str)
+  );
+}
+
+var trimWhiteSpace = function(str) {
+  return str.replace(/(^[\s]+|[\s]+$)/g, '');
+}
+
+var killFunnyDash = function(film) {
+  return film.replace("–", '');
+}
 
 applyRules = function(film) {
   return rules.reduce((film, rule) => film.replace(rule, ''), film);
@@ -17,8 +35,12 @@ var reduceFilms = ((lastFilm) => (results, film, i) => {
   var input = film;
   if (!skipFilm(lastFilm, film)) {
     var film = applyRules(film);
-    results.films.push([film, input]);
-    lastFilm = film;
+    film = killFunnyDash(film);
+    film = trimWhiteSpace(film);
+    if (film !== lastFilm && !isShow(film)) {
+      results.films.push([film, input]);
+      lastFilm = film;
+    }
   }
   return results;
 })('');
@@ -45,12 +67,24 @@ var testFilms = [
   '    ',
   'film2',
   '42ND STREET*',
+  '42ND STREET (brackets)',
   '666  PROPHECY  THE',
   '6TH DAY - THE',
   '8 WOMEN (8 FEMMES)',
   'AGONY AND THE ECSTASY -THE-',
   'AMERICAN HAUNTING  AN',
-  'ASSAULT  THE (2010)'
+  'ASSAULT  THE (2010)',
+  'BEST MAN - THE',
+  'BEST MEN – THE',
+  'DEGREE OF MURDER -A-',
+  'DISNEY COMPILATION 3(HERESGOOF',
+  'DUCK SOUP (MARX BROTHERS)*',
+  'DUELLISTS -THE-*',
+  'E.R VOL 1 SERIES 1 EPS 2 & 3',
+  'EASY COME  EASY GO (1947) (UNI)',
+  'FAIL SAFE ***WB***',
+  'FRIENDS: COMPLETE SEASON 10',
+  'BABE  -'
 ];
 
 var expected = [
@@ -59,10 +93,19 @@ var expected = [
   '42ND STREET',
   '666  PROPHECY',
   '6TH DAY',
-  '8 WOMEN ',
+  '8 WOMEN',
   'AGONY AND THE ECSTASY',
   'AMERICAN HAUNTING',
-  'ASSAULT'
+  'ASSAULT',
+  'BEST MAN',
+  'BEST MEN',
+  'DEGREE OF MURDER',
+  'DISNEY COMPILATION 3',
+  'DUCK SOUP',
+  'DUELLISTS',
+  'EASY COME  EASY GO',
+  'FAIL SAFE',
+  'BABE'
 ];
 
 var testResult = cleanFilms(testFilms);
@@ -75,6 +118,9 @@ expected.forEach((film, i) => {
   if (film !== testResult.films[i][0]) throw 'fail at ' + film + ' : ' + testResult.films[i][0];
 });
 
-showFilms(results, 930, 30);
+showFilms(results, 990, 30);
 
-fs.writeFileSync('cleanFilms.csv', results.films.map(pair => pair[0]).join('\n'));
+fs.writeFileSync('cleanFilms.csv', results.films.map(pair => pair[0]).join('\n'), 'utf8');
+
+var filmsStr = fs.readFileSync('cleanFilms.csv', 'utf8').toString().slice(0, 2000);
+console.log(filmsStr);
